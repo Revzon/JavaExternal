07254b0d0a08c9d7d5154dbfad09a2d60fe24ff6
olga <revzono!gmail.com>
2018-03-22 12:58:33 +0200

    Improve second sort of the array

--- lesson1/task1/src/Task1Main.java
+++ lesson1/task1/src/Task1Main.java
@@ -1,10 +1,12 @@
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 /**
  * Created by olga on 11.03.18.
  */
+
 
 public class Task1Main {
 
@@ -25,10 +27,7 @@ public class Task1Main {
     }
 
 
-
     private static int[] getArray(int k, Scanner s) {
-
-        int choice;
 
         System.out.println();
         System.out.println("1. Input array");
@@ -38,7 +37,7 @@ public class Task1Main {
         while (true) {
 
             System.out.println("Your choice...");
-            choice = s.nextInt();
+            int choice = s.nextInt();
 
             switch (choice) {
                 case (1):
@@ -55,9 +54,7 @@ public class Task1Main {
     }
 
 
-
-    private static int[] insertArray(int k, Scanner s){
-
+    private static int[] insertArray(int k, Scanner s) {
         int[] arr = new int[k];
         System.out.println("Please enter the elements: ");
         for (int i = 0; i < k; i++) {
@@ -66,7 +63,6 @@ public class Task1Main {
 
         return arr;
     }
-
 
 
     private static int[] generateArray(int k) {
@@ -81,13 +77,13 @@ public class Task1Main {
         }
 
         System.out.println("Generated array: ");
-        printUnivariateArray(arr);
+        System.out.println(Arrays.toString(arr));
 
         return arr;
     }
 
 
-    private static void sortByFirstCriterion(int[] arr){
+    private static void sortByFirstCriterion(int[] arr) {
 
         int temp = 0;
         for (int i = 0; i < arr.length; i++) {
@@ -97,9 +93,8 @@ public class Task1Main {
                 if (((arr[i] >= 0) && (arr[j] < 0))
                         || ((arr[i] >= 0) && (arr[j] >= 0) && (arr[i] < arr[j]))
                         || ((arr[i] < 0) && (arr[j] < 0) && (arr[i] > arr[j]))) {
-                        temp = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = temp;
+
+                    swap(arr, i, j);
                 }
 
             }
@@ -107,45 +102,40 @@ public class Task1Main {
         }
 
         System.out.println("First criterion:");
-
-        printUnivariateArray(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
 
+    private static void sortBySecondCriterion(int[] arr) {
 
-    private static void sortBySecondCriterion(int[] arr){
+        int j = arr.length - 1;
+        int i = 0;
 
-        int temp = 0;
-        for (int i = 0; i < arr.length; i++) {
+        while (i < j) {
 
-            for (int j = i; j < arr.length; j++) {
+            if (arr[i] < 0) {
 
-                if (((arr[i] <= 0) && (arr[j] > 0)) ||
-                    ((arr[i] < 0) && (arr[j] == 0)))
-                {
-                    temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+                while ((arr[j] < 0) && (j > i)) {
+                    j--;
                 }
 
+                swap(arr, i, j);
             }
 
+            i++;
         }
 
         System.out.println("Second criterion:");
-        printUnivariateArray(arr);
+        System.out.println(Arrays.toString(arr));
 
     }
 
 
+    private static void swap(int[] arr, int i, int j) {
 
-    private static void printUnivariateArray(int[]arr){
-
-       for (int i = 0; i < arr.length; i++) {
-           System.out.print(arr[i] + "  ");
-
-       }
-        System.out.println();
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
 
     }
 
